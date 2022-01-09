c5c47206be228d392dfac5562c71d87c7d6e316a
Daniel Baraniak <230443@edu.p.lodz.pl>
2022-01-09 01:45:13 +0100

    Separate Api().dio and token_repository

---
+++ frontend/lib/api/token_repository.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenRepository {
+  static const storage = FlutterSecureStorage();
+
+  static String? _accessToken;
+
+  static Future<String?> getAccessToken() async {
+    _accessToken ??= await storage.read(key: 'accessToken');
+    return _accessToken;
+  }
+
+  static Future<void> setAccessToken(String? value) async {
+    _accessToken = value;
+    await storage.write(key: 'refreshToken', value: value);
+  }
+
+  static Future<String?> getRefreshToken() async =>
+      await storage.read(key: 'refreshToken');
+
+  static Future<void> setRefreshToken(String? value) async =>
+      await storage.write(key: 'refreshToken', value: value);
+}
