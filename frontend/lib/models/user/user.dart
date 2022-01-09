fdfa0ec77bea665386bfebfd0b99d3ffd1ea20a3
Daniel Baraniak <230443@edu.p.lodz.pl>
2022-01-09 21:45:56 +0100

    Improve models. Add tests

--- frontend/lib/models/user/user.dart
+++ frontend/lib/models/user/user.dart
@@ -26,4 +26,9 @@ class User {
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
   Map<String, dynamic> toJson() => _$UserToJson(this);
+
+  @override
+  String toString() {
+    return toJson().toString();
+  }
 }
