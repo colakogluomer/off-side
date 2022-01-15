3c68d71f780a2b2a659e76099e01ff6b17fdfa4c
Daniel Baraniak <230443@edu.p.lodz.pl>
2022-01-15 23:10:04 +0100

    Refactor code.

--- frontend/lib/widgets/user_card.dart
+++ frontend/lib/widgets/user_card.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/constants/settings.dart' as settings;
+import 'package:frontend/models/team/team.dart';
 import 'package:frontend/models/user/user.dart';
+import 'package:frontend/services/team_service.dart';
+import 'package:frontend/widgets/team_card.dart';
 import 'package:frontend/widgets/user_screen.dart';
 
-class UserCard extends StatelessWidget {
-  const UserCard({
+class UserHorizontalCard extends StatelessWidget {
+  const UserHorizontalCard({
     required this.user,
     Key? key,
   }) : super(key: key);
@@ -37,6 +40,65 @@ class UserCard extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class UserStackedCard extends StatelessWidget {
+  const UserStackedCard({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 300,
+            child: user.profileImage != null
+                ? Image.network(
+                    '${settings.serverUrl}/uploads/users/${user.profileImage}',
+                    fit: BoxFit.fitWidth,
+                  )
+                : Image.asset(
+                    'images/avatar_placeholder.png',
+                    fit: BoxFit.fitWidth,
+                  ),
+          ),
+          const SizedBox(height: 15.0),
+          ListTile(
+            title: Text(user.name),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('position'),
+                Text('level'),
+              ],
+            ),
+          ),
+          if (user.teamId != null)
+            FutureBuilder<Team?>(
+              future: TeamService.getById(user.teamId ?? "no team"),
+              builder: (BuildContext context, AsyncSnapshot<Team?> snapshot) {
+                Widget child;
+                Team? userTeam = snapshot.data;
+                debugPrint(snapshot.hasData.toString());
+                if (snapshot.hasData && userTeam != null) {
+                  child = TeamCard(team: userTeam);
+                } else {
+                  child = Container();
+                }
+                return child;
+              },
+            ),
+        ],
       ),
     );
   }
--- frontend/lib/widgets/user_screen.dart
+++ frontend/lib/widgets/user_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/constants/settings.dart' as settings;
-import 'package:frontend/models/team/team.dart';
 import 'package:frontend/models/user/user.dart';
-import 'package:frontend/services/team_service.dart';
-import 'package:frontend/widgets/team_card.dart';
+import 'package:frontend/widgets/user_card.dart';
 
 class UserProfileScreen extends StatelessWidget {
   static const routeName = '/creations_library/video_meme_creator';
@@ -20,65 +17,23 @@ class UserProfileScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Card(
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 300,
-                child: user.profileImage != null
-                    ? Image.network(
-                        '${settings.serverUrl}/uploads/users/${user.profileImage}',
-                        fit: BoxFit.fitWidth,
-                      )
-                    : Image.asset(
-                        'images/avatar_placeholder.png',
-                        fit: BoxFit.fitWidth,
-                      ),
-              ),
-              const SizedBox(height: 15.0),
-              ListTile(
-                title: Text(user.name),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('position'),
-                    Text('level'),
-                  ],
+        child: Column(
+          children: [
+            UserStackedCard(user: user),
+            ButtonBar(
+              alignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () => {},
+                  child: const Text("Contact"),
                 ),
-              ),
-              if (user.teamId != null)
-                FutureBuilder<Team?>(
-                  future: TeamService.getById(user.teamId ?? "no team"),
-                  builder:
-                      (BuildContext context, AsyncSnapshot<Team?> snapshot) {
-                    Widget child;
-                    Team? userTeam = snapshot.data;
-                    debugPrint(snapshot.hasData.toString());
-                    if (snapshot.hasData && userTeam != null) {
-                      child = TeamCard(team: userTeam);
-                    } else {
-                      child = Container();
-                    }
-                    return child;
-                  },
+                ElevatedButton(
+                  onPressed: () => {},
+                  child: const Text("Invite"),
                 ),
-              ButtonBar(
-                alignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => {},
-                    child: const Text("Contact"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => {},
-                    child: const Text("Invite"),
-                  ),
-                ],
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
