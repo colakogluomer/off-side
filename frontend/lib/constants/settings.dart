9d6f6b1163f3b20672d1c27e7038f4fe91f44eda
Daniel Baraniak <230443@edu.p.lodz.pl>
2022-01-26 19:24:42 +0100

    change server URL.

--- frontend/lib/constants/settings.dart
+++ frontend/lib/constants/settings.dart
@@ -1,2 +1,3 @@
-const String serverUrl = "http://10.0.2.2:8080";
+//const String serverUrl = "http://10.0.2.2:8080"; // Use for local environment
+const String serverUrl = "https://offside-backend.herokuapp.com";
 const String refreshEndpoint = "/users/login";
